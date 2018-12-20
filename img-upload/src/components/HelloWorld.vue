3e9529992ff214c9e7e90c221eb9bcaf3a7b64a3
hexiuhui <hexiuhui@tvflnet.com>
2018-12-20 11:12:43 +0800

    添加部分依赖

---
+++ img-upload/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <img-upload></img-upload>
+    </div>
+</template>
+
+<script>
+import imgUpload from './upload'
+export default {
+    name: "HelloWorld",
+    components:{ imgUpload },
+    data() {
+        return {};
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ img-upload/src/components/upload.vue
@@ -0,0 +1,8 @@
+<template>
+    <Input placeholder="写点什么吧" style="width: 300px"></Input>
+</template>
+<script>
+    export default {
+        name:'imgUpload'
+    }
+</script>
